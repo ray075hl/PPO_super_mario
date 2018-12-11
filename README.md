af03e67238886b1e989953ecec0889d78bfaa135
ray075hl <znzx075hl@gmail.com>
2018-12-11 15:49:32 +0800

    Begin code refactoring

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PPO_super_mario
 Play game super mario using Proximal Policy Optimization method.
+
+
+
+
+
+## reference
+
+\[1\]  [Proximal Policy Optimization](https://arxiv.org/abs/1707.06347)
+
+\[2\]  [http://blog.varunajayasiri.com/ml/ppo.html](http://blog.varunajayasiri.com/ml/ppo.html) This great post help me a lot. It tell me how to warp a game   
+
+​      like deepmind done with atari game.
+
+\[3\]  [Game environment](https://github.com/Kautenja/gym-super-mario-bros)
+
+\[4\]  [PPO tutorial code](https://github.com/higgsfield/RL-Adventure-2) That is a very clean project and friendly for newbie rl algorithm learner. I borrow  
+
+​      part code from it.
+
+ 
---
+++ src/monitor.py
@@ -0,0 +1,35 @@
+import subprocess as sp
+import numpy as np
+
+
+FFMPEG_BIN = 'ffmpeg'
+
+
+class Monitor:
+    def __init__(self, width, height, save_path='output_video.mp4'):
+        
+        self.command = [ FFMPEG_BIN,
+                '-y', # (optional) overwrite output file if it exists
+                '-f', 'rawvideo',
+                '-vcodec','rawvideo',
+                '-s', '{}X{}'.format(width, height), # size of one frame
+                '-pix_fmt', 'rgb24',
+                '-r', '60', # frames per second
+                '-i', '-', # The imput comes from a pipe
+                '-an', # Tells FFMPEG not to expect any audio
+                '-vcodec', 'mpeg4',
+                save_path ]
+
+        self.pipe = sp.Popen( self.command, stdin=sp.PIPE, stderr=sp.PIPE)
+
+    def record(self, image_array):
+        self.pipe.stdin.write( image_array.tostring() )
+
+
+# unit test
+if __name__ == '__main__':
+    width, height = 640, 480
+    monitor = Monitor(width, height)
+    for _ in range(100):
+        frame = np.zeros((height, width, 3))
+        monitor.record(frame)
